011354c9284f1aeb182a4e2423b993b49d8ac9b0
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2016-02-29 18:56:04 +0100

    View: show as table

--- src/pl/kkoscielniak/rpn/controller/Controller.java
+++ src/pl/kkoscielniak/rpn/controller/Controller.java
@@ -4,6 +4,7 @@ import java.util.EmptyStackException;
 import java.util.List;
 import pl.kkoscielniak.rpn.model.ReversePolishNotation;
 import pl.kkoscielniak.rpn.model.StackState;
+import pl.kkoscielniak.rpn.view.View;
 
 /**
  * Responsible for interacting with the model 
@@ -16,17 +17,15 @@ public class Controller {
      * Method responsible for the work flow and catching existing exceptions.
      * @param args the command line arguments
      */
-    
     public static void main(String[] args) {
         
         ReversePolishNotation rpn = new ReversePolishNotation();
+        View view = new View();
         
         try {
             List<StackState> evaluatedStackStates = rpn.CalculateRPN(args);
             
-            for (StackState evaluatedStackState : evaluatedStackStates) {
-                System.out.println(evaluatedStackState.getStack());
-            }
+            view.printTable(evaluatedStackStates);
             
         } catch (EmptyStackException e) {
             System.out.println("The stack is empty!");
--- src/pl/kkoscielniak/rpn/view/View.java
+++ src/pl/kkoscielniak/rpn/view/View.java
@@ -1,9 +1,65 @@
 package pl.kkoscielniak.rpn.view;
 
+import java.util.List;
+import java.util.Scanner;
+import pl.kkoscielniak.rpn.model.StackState;
+
 /**
  * Responsible for maintaining the presentation layer - the view. 
  * @author krystian
  */
 public class View {
+    /**
+     * Represents instance of Scanner object, which is responsible for 
+     * collecting information from user.
+     */
+    private final Scanner scanner = new Scanner(System.in);
     
+    /**
+     * The contructor takes no arguments.
+     */
+    public View() {
+        
+    }
+    
+    /**
+     * Prompts for equation if the user didn't pass it as an arguments.
+     * @return input written by the user
+     */
+    public String getTheEquation() {
+        System.out.print("Put the equation here: ");
+        return scanner.nextLine();
+    }
+    
+    /**
+     * Prints the given message
+     * @param msg message to print
+     */
+    public void printMessage(String msg) {
+        System.out.print(msg);
+    }
+    
+    /**
+     * Prints the table of steps to calculate the result.
+     * @param stackStates list of saved stack states to be shown 
+     */
+    public void printTable(List<StackState> stackStates) {
+        String leftAlignFormat = "| %-4s | %-30s | %-4s |%n";
+
+        System.out.format("+------+--------------------------------+------+%n");
+        System.out.format("+ Step | Current stack                  | Op   |%n");
+        System.out.format("+------+--------------------------------+------+%n");
+        
+        
+        int i = 1;        
+        for (StackState stackState : stackStates) {
+            System.out.format(leftAlignFormat, 
+                    i,
+                    stackState.getStack(), 
+                    stackState.getOperator());
+            
+            i++;
+        }
+        System.out.format("+------+--------------------------------+------+%n");
+    }
 }
